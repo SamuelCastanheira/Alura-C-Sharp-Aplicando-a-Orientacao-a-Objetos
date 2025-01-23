ae4ecdc6b2dac0b30f2cd748a85bb75202b7bf3d
Samuel Castanheira <samuel.rocha.castan@gmail.com>
2025-01-23 12:47:41 -0300

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   Musica.cs
    	new file:   Program.cs
    Capítulos 1 e 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 300;
+musica1.Disponivel = true;
+musica1.ExibirFichaTecica();
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo"; musica2.Artista = "U2";
+musica2.Duracao = 350;
+musica2.Disponivel = false;
+musica2.ExibirFichaTecica();
+musica2.ExibirInfos();
+
